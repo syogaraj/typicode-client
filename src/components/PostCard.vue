95098621b44b835a8ca150226b372c2aa28e3e59
Yogaraj <yoga30696@gmail.com>
2019-10-25 23:50:34 +0530

    addded hover and icons for better viewing experience

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,19 +1,27 @@
 <template>
     <v-row dense>
         <v-col cols="3" class="pa-5" v-for="postData in postMeta" :key="postData.postId">
-            <v-card max-width="400">
-                <v-card-text>
-                    <div>Post Name: {{ postData.POST_ID }}</div>
-                    <div class="text--primary">Comments: {{ postData.COUNT }}</div>
-                </v-card-text>
-                <v-card-actions>
-                    <v-btn
-                        text
-                        color="primary accent-4"
-                        @click="$emit('show-post-info', postData.POST_ID)"
-                    >Show more</v-btn>
-                </v-card-actions>
-            </v-card>
+            <v-hover v-slot:default="{ hover }">
+                <v-card max-width="300" :elevation="hover ? 12 : 2">
+                    <v-card-text>
+                        <div>
+                            <v-icon class="pr-2">dvr</v-icon>
+                            Post Name: {{ postData.POST_ID }}
+                        </div>
+                        <div class="text--primary">
+                            <v-icon class="pr-2">message</v-icon>
+                            Comments: {{ postData.COUNT }}
+                        </div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn
+                            text
+                            color="primary accent-4"
+                            @click="$emit('show-post-info', postData.POST_ID)"
+                        >Show more</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-hover>
         </v-col>
     </v-row>
 </template>
