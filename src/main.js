c663cc7a8b29a30c86906bce3e5bfebdf5f6676d
Yogaraj <yoga30696@gmail.com>
2019-10-25 22:22:09 +0530

    added axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import axios from 'axios';
 
 Vue.config.productionTip = false
+
+//axios config
+const axiosConfig = {
+  "baseURL": "http://localhost:3000/",
+  "timeout": 10000
+}
+
+Vue.prototype.$http = axios.create(axiosConfig);
 
 new Vue({
   vuetify,
