3dea4ce7736998a292b009b70a0cd7f80d5c60fd
Yogaraj <yoga30696@gmail.com>
2019-10-25 23:49:52 +0530

    added post card details

---
+++ src/components/PostDetails.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <v-dialog v-model="dialog" max-width="600px" @input="restore()">
+            <v-list>
+                <v-subheader>User's view on this post</v-subheader>
+                <v-list-item-group
+                    v-for="(detail, index) in postDetails"
+                    :key="index"
+                    class="pl-5 pb-5"
+                >
+                    <!-- <v-list-item-content> -->
+                    <v-list-item-title>
+                        <span>
+                            <v-subheader class="pa-1">
+                                <v-icon color="indigo" class="pr-2">person_pin</v-icon>
+                                {{ detail.NAME }}:
+                            </v-subheader>
+                        </span>
+                    </v-list-item-title>
+                    <v-list-item-subtitle
+                        v-html="detail.BODY"
+                        style="background: #dad9d9; border-radius: 1vw; padding: 1vw;"
+                    ></v-list-item-subtitle>
+                    <!-- </v-list-item-content> -->
+                </v-list-item-group>
+            </v-list>
+        </v-dialog>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        postID: {
+            type: Number,
+            default: () => 0
+        },
+        postDetails: {
+            type: Array,
+            default: () => []
+        }
+    },
+    data() {
+        return {
+            dialog: true
+        };
+    },
+    methods: {
+        restore() {
+            this.$emit("restore");
+        }
+    }
+};
+</script>
+
+<style>
+</style>
