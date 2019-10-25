d408eeb36f9011b1205a2ed07235156f75d46b36
Yogaraj <yoga30696@gmail.com>
2019-10-25 23:51:01 +0530

    added dark mode and PostDetails view

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
     <v-app>
         <div app>
-            <v-toolbar dense>
+            <v-toolbar dense :dark="dark">
                 <v-toolbar-title>ORMAE</v-toolbar-title>
             </v-toolbar>
 
             <v-content>
-                <PostCard :post-meta="postInfo"></PostCard>
+                <PostCard :post-meta="postInfo" @show-post-info="fetchPostDetails(...arguments)"></PostCard>
+                <PostDetails
+                    :post-id="postID"
+                    :post-details="postDetails"
+                    v-if="postID > 0"
+                    @restore="restore()"
+                ></PostDetails>
             </v-content>
         </div>
     </v-app>
@@ -15,21 +21,40 @@
 <script>
 // import HelloWorld from "./components/HelloWorld";
 import PostCard from "./components/PostCard";
+import PostDetails from "./components/PostDetails";
 
 export default {
     name: "App",
     components: {
         // HelloWorld
-        PostCard
+        PostCard,
+        PostDetails
     },
     data: () => ({
-        postInfo: {}
+        postInfo: [],
+        postID: 0,
+        postDetails: [],
+        dark: true
     }),
+    methods: {
+        fetchPostDetails(postID) {
+            let handle = this;
+            //eslint-disable-next-line
+            console.log(postID);
+            this.postID = postID;
+            this.$http.get("/getPostDetails?ID=" + postID).then(response => {
+                handle.postDetails = response.data;
+            });
+        },
+        restore() {
+            this.postID = 0;
+        }
+    },
     mounted() {
-      let handle = this;
-      this.$http.get('/getpost').then(response => {
-        handle.postInfo = response.data;
-      });
+        let handle = this;
+        this.$http.get("/getpost").then(response => {
+            handle.postInfo = response.data;
+        });
     }
 };
 </script>
